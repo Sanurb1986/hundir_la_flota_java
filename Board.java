4336d6539b1de24b737e2598450f668f2f7693b4
Santiago Urbano Bedoya <santiagourbao@gmail.com>
2026-01-28 16:15:12 +0100

    Creacion tablero

--- Board.java
+++ Board.java
@@ -1,5 +1,34 @@
 public class Board {
-    public void display() {
+    public static final int SIZE = 10;
+    public static char[][] tablero;
+    public static final char EMPTY = '~';
+    public static final char SHIP = 'S';
+
+    public static void inicializarTablero() {
+        tablero = new char[SIZE][SIZE];
+
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                tablero[i][j] = EMPTY; // Agua
+            }
+        }
+    }
+
+    public static void display() {
         System.out.println("cargando tablero...");
+    }
+
+    public static void placeShip(int x, int y, int length, boolean horizontal) {
+        System.out.println("colocando barco en el tablero...");
+        
+        if (horizontal) {
+            for (int i = 0; i < length; i++) {
+                tablero[x][y + i] = SHIP;
+            }
+        } else {
+            for (int i = 0; i < length; i++) {
+                tablero[x + i][y] = SHIP;
+            }
+        }
     }
 }
